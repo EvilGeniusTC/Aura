b000c37dec83518737e7925800592e52f6413430
EvilGeniusTC <traviscraig.main@gmail.com>
2024-07-22 00:56:59 -0400

    Started UI System WidgetController

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright BINARY REALMS
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
